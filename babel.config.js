44b54acd335e39af576e1ba252fa79181de4698b
wzm507 <15305443429@163.com>
2025-10-11 10:17:37 +0800

    添加 @babel/preset-typescript 到 Babel 配置

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ module.exports = {
   presets: [
     '@babel/preset-env',
     '@babel/preset-react'
+    '@babel/preset-typescript'
   ],
   plugins: [
     '@babel/plugin-transform-unicode-property-regex'
